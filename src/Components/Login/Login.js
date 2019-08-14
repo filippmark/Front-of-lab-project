5ef468ac85c61af7e4f13786b3349011ba084edb
Filip Markovich <Filip.Markovich@itechart-group.com>
2019-08-14 16:18:06 +0300

    features added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,25 +1,41 @@
 import React, {Component} from 'react';
 import "./Login.css"
+import { DEFAULT_ECDH_CURVE } from 'tls';
 
 class Login extends Component{
 
     
     state = {
-        name: "",
-        pass: ""
+        userName: "",
+        password: ""
+    }
+
+
+    handleChange = (event) => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault();
+
+        console.log(this.state);
     }
 
     render(){
         
-
         return(
             <div className = "login">
                 <form>
-                    <label> Ваш логин или email </label>
-                    <input type = "text" id = "userName"/>
+                    <label> Ваш email </label>
+                    <div>
+                        <input type = "text" id = "email" name = "userName"  onChange = {this.handleChange} required/>
+                    </div>
                     <label> Пароль </label>
-                    <input type ="password" id = "password"/>
-                    <input type = "submit" id = "btn" value = "Войти"/>
+                    <input type ="password" id = "password" name = "password" onChange = {this.handleChange} required/>
+                    <input type = "submit" id = "btn" value = "Войти" onClick = {this.handleSubmit}/>
                 </form>
             </div>
         )    
